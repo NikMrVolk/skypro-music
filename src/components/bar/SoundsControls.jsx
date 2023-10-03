da8fb498d7933afdb36e80ba06f4be05e2842601
NickZavadski <nick.mr.volk@gmail.com>
2023-10-03 12:13:46 +0300

    add render sounds controllers by map

--- src/components/bar/SoundsControls.jsx
+++ src/components/bar/SoundsControls.jsx
@@ -1,39 +1,28 @@
 import * as SC from '../../styles/common'
+import barSoundsControllers from '../../data/barSoundsControllers'
 
 const SoundsControls = () => {
 	return (
 		<SC.Flex $padding="0 27px 0 31px">
-			<SC.Flex $margin="0 23px 0 0" $padding="5px" $alignC>
-				<SC.Svg $height="14px" $width="15px" alt="prev">
-					<use xlinkHref="img/icon/sprite.svg#icon-prev" />
-				</SC.Svg>
-			</SC.Flex>
-			<SC.Flex $margin="0 23px 0 0" $padding="5px" $alignC className="_btn">
-				<SC.Svg $height="20px" $width="22px" $fill="#d9d9d9" alt="play">
-					<use xlinkHref="img/icon/sprite.svg#icon-play" />
-				</SC.Svg>
-			</SC.Flex>
-			<SC.Flex $margin="0 28px 0 0" $padding="5px" $alignC>
-				<SC.Svg $height="14px" $width="15px" $fill="inherit" $stroke="#d9d9d9" alt="next">
-					<use xlinkHref="img/icon/sprite.svg#icon-next" />
-				</SC.Svg>
-			</SC.Flex>
-			<SC.Flex $margin="0 24px 0 0" $padding="5px" $alignC className="_btn-icon">
-				<SC.Svg $height="12px" $width="18px" $fill="transparent" $stroke="#696969" alt="repeat">
-					<use xlinkHref="img/icon/sprite.svg#icon-repeat" />
-				</SC.Svg>
-			</SC.Flex>
-			<SC.Flex $padding="5px" $alignC className="_btn-icon">
-				<SC.Svg
-					$height="12px"
-					$width="19px"
-					$fill="transparent"
-					$stroke="#696969"
-					alt="shuffle"
+			{barSoundsControllers.map((controller) => (
+				<SC.Flex
+					key={controller.src}
+					$margin={controller.mrWr}
+					className={controller.class}
+					$padding="5px"
+					$alignC
 				>
-					<use xlinkHref="img/icon/sprite.svg#icon-shuffle" />
-				</SC.Svg>
-			</SC.Flex>
+					<SC.Svg
+						$height={controller.height}
+						$width={controller.width}
+						$fill={controller.fill}
+						$stroke={controller.stroke}
+						alt={controller.alt}
+					>
+						<use xlinkHref={controller.src} />
+					</SC.Svg>
+				</SC.Flex>
+			))}
 		</SC.Flex>
 	)
 }
