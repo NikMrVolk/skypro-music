665237241c3bd2da3e6c9a770a7d69319dc031be
NickZavadski <nick.mr.volk@gmail.com>
2023-10-28 15:41:59 +0300

    add track bubble

--- src/components/center/SongItem.jsx
+++ src/components/center/SongItem.jsx
@@ -1,30 +1,32 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import * as SC from '../../styles/common'
 import DurationTime from './DurationTime'
 import { setSong } from '../../store/reducers/sounds'
+import { TrackBubble } from '../../styles/trackBubble'
 
 const SongItem = ({ id, name, subTitle, author, album, duration_in_seconds }) => {
+	const { song, playing } = useSelector((state) => state.songs)
 	const dispatch = useDispatch()
-
-	const play = (id) => {
-		dispatch(setSong(id))
-	}
 
 	return (
 		<SC.Block $w="100%" $mB="12px">
 			<SC.Flex $row $jstSB $alignC>
 				<SC.Flex $w="447px" $row $alignC>
 					<SC.Flex $h="51px" $w="51px" $p="16px" $mR="17px" $back="#313131" $jstC $alignC>
-						<SC.Svg $h="17px" $w="18px" $fill="transparent" $stroke="#4e4e4e" alt="music">
-							<use xlinkHref="img/icon/sprite.svg#icon-note" />
-						</SC.Svg>
+						{song.id === id ? (
+							<TrackBubble $active={playing} />
+						) : (
+							<SC.Svg $h="17px" $w="18px" $fill="transparent" $stroke="#4e4e4e" alt="music">
+								<use xlinkHref="img/icon/sprite.svg#icon-note" />
+							</SC.Svg>
+						)}
 					</SC.Flex>
 					<SC.Block
 						$color="#ffffff"
 						$point="pointer"
 						onClick={() => {
-							play(id)
+							dispatch(setSong(id))
 						}}
 					>
 						{name} <SC.Span $color="#4e4e4e">{subTitle}</SC.Span>
