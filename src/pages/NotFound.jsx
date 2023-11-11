636dbdac50d68849d0004d4d9a057813b2870031
NickZavadski <nick.mr.volk@gmail.com>
2023-11-11 20:25:40 +0300

    some changes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,13 @@
+import { useNavigate } from 'react-router'
+import { LOGIN_ROUTE, MAIN_ROUTE } from '../utils/constants'
+
 const NotFound = () => {
+	const navigate = useNavigate()
+	if (localStorage.getItem('user')) {
+		navigate(MAIN_ROUTE)
+	} else {
+		navigate(LOGIN_ROUTE)
+	}
 	return <div style={{ color: 'red', fontSize: '60px' }}>Not found</div>
 }
 
